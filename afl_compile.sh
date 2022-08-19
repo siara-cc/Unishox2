97e2c1cf10001ce7bb7f7c36fc6f89902e699ff7
Arun <arun@siara.cc>
2022-08-19 15:14:30 +0530

    Add afl supporting files

--- afl_compile.sh
+++
@@ -1,2 +0,0 @@
-afl-clang -DUNISHOX_API_WITH_OUTPUT_LEN=1 -g -fsanitize=address unishox2.c test_fuzz.c
-
--- afl_run.sh
+++
@@ -1,1 +0,0 @@
-mkdir -p fuzz-data && echo '       ' > fuzz-data/SEED && afl-fuzz -m none -i fuzz-data -o fuzz-out ./a.out @@
